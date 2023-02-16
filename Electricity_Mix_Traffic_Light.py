10640fc43f3b166aef1e7419187059dfb47853a8
Sven Luzar <{ID}+{username}@users.noreply.github.com>
2023-02-16 16:21:26 +0100

    json format and command line args added

--- Electricity_Mix_Traffic_Light.py
+++ Electricity_Mix_Traffic_Light.py
@@ -3,6 +3,7 @@ import pandas as pd
 from datetime import date, timedelta, datetime
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
 
 def download_load_data(country_code, start, end):
@@ -17,7 +18,7 @@ def download_load_data(country_code, start, end):
 
     return pd_demand, pd_wind_solar_load
 
-def calculate_share_of_renewables(pd_demand, pd_wind_solar_load, index=0, print_value=False):
+def calculate_share_of_renewables(pd_demand, pd_wind_solar_load, index=0, print_value=False, format='text'):
     # Share of Renewables
     demand = pd_demand['Forecasted Load'][index]
     wind_solar_load = pd_wind_solar_load['Solar'][index] + pd_wind_solar_load['Wind Onshore'][index]
@@ -27,18 +28,29 @@ def calculate_share_of_renewables(pd_demand, pd_wind_solar_load, index=0, print_
     share_of_renewables = wind_solar_load / demand
 
     if print_value is True:
-        print('Net Load: ' + str(pd_demand['Forecasted Load'][index]) + ' MW')
-        print('Solar: ' + str(pd_wind_solar_load['Solar'][index]) + ' MW')
-        if 'Wind Offshore' in pd_wind_solar_load.columns.values:
-            print('Wind Offshore: ' + str(pd_wind_solar_load['Wind Offshore'][index]) + ' MW')
-        print('Wind Onshore: ' + str(pd_wind_solar_load['Wind Onshore'][index]) + ' MW')
+        if format == 'text':
+            print('Net Load: ' + str(pd_demand['Forecasted Load'][index]) + ' MW')
+            print('Solar: ' + str(pd_wind_solar_load['Solar'][index]) + ' MW')
+            if 'Wind Offshore' in pd_wind_solar_load.columns.values:
+                print('Wind Offshore: ' + str(pd_wind_solar_load['Wind Offshore'][index]) + ' MW')
+            print('Wind Onshore: ' + str(pd_wind_solar_load['Wind Onshore'][index]) + ' MW')
+        elif format == 'json':
+            print('"netLoad": ' + str(pd_demand['Forecasted Load'][index]) + ', ')
+            print('"solar": ' + str(pd_wind_solar_load['Solar'][index]) + ', ')
+            if 'Wind Offshore' in pd_wind_solar_load.columns.values:
+                print('"windOffshore": ' + str(pd_wind_solar_load['Wind Offshore'][index]) + ', ')
+            print('"windOnshore": ' + str(pd_wind_solar_load['Wind Onshore'][index]) + ", ")
+
 
     return share_of_renewables
 
-def calculate_current_share_of_renewables(country_code):
+def calculate_current_share_of_renewables(country_code, format):
     # Extract Timestamps
     timestamp_now = datetime.now()  # .strftime("%Y%m%d%H%M")
-    print(timestamp_now)
+    if format == 'text':
+        print(timestamp_now)
+    elif format == 'json':
+        print('"timestamp_now": "' + str(timestamp_now.isoformat()) + '", ')
     timestamp_near_future = (datetime.now() + timedelta(minutes=15))  # .strftime("%Y%m%d%H%M")
 
     #Calculate the current share of renewables
@@ -46,11 +58,11 @@ def calculate_current_share_of_renewables(country_code):
                                                        start=timestamp_now,
                                                        end=timestamp_near_future)
     current_share_of_renewables = calculate_share_of_renewables(pd_demand, pd_wind_solar_load, index=0,
-                                                       print_value=True)
+                                                       print_value=True, format=format)
 
     return current_share_of_renewables
 
-def calculate_share_of_renewable_quantiles(country_code, no_of_quantiles, days_in_past, days_in_future, today_mode):
+def calculate_share_of_renewable_quantiles(country_code, no_of_quantiles, days_in_past, days_in_future, today_mode, format):
     # Downloads data
     if today_mode==True:
         pd_demand, pd_wind_solar_load = download_load_data(country_code=country_code,
@@ -70,7 +82,22 @@ def calculate_share_of_renewable_quantiles(country_code, no_of_quantiles, days_i
         renewables_dict[pd_demand.index[i]] = share_of_renewables
 
     renewables_df = pd.DataFrame(renewables_dict.values(),index=renewables_dict.keys())
-    print(renewables_df)
+
+    if format == 'text':
+        print(renewables_df)
+    elif format == 'json':
+        print('"history": [')
+        for label, series in renewables_df.items():
+            #print('"' + str(label) + '": "' + str(series) + '", ')
+            first = True
+            for index, value in series.items():
+                if first == False:
+                    print(",")
+                print('{ "timestamp": "' + str(index.isoformat()) + '", "value": ' + str(value) +  ' }', end='')
+                first = False
+
+        print()
+        print('],')
     # print(list_of_renewables)
 
     # Calculates quantiles
@@ -109,7 +136,8 @@ def main_app(token='xxx',
              days_in_past=5,
              days_in_future=5,
              today_mode=True,
-             plotting=True):
+             plotting=True,
+             format='text'):
 
     global client
     client = EntsoePandasClient(api_key=token)
@@ -125,34 +153,52 @@ def main_app(token='xxx',
     ##############################
     ##############################
 
+    if format == 'json':
+        print('{')
+
     # Calculate the current share of renewables
-    current_share_of_renewables = calculate_current_share_of_renewables(country_code)
+    current_share_of_renewables = calculate_current_share_of_renewables(country_code, format)
     # Calculate quantiles of past and future
     quantiles, renewables_df = calculate_share_of_renewable_quantiles(country_code, no_of_quantiles,
                                                        days_in_past,
                                                        days_in_future,
-                                                       today_mode=today_mode)
+                                                       today_mode=today_mode,
+                                                       format=format)
     # Calculate current traffic light color
     color = calculate_traffic_light_color(current_share_of_renewables,quantiles,color_scheme)
 
-    print('Quantile: ' + str(quantiles))
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    print('REGION: '+str(country_code))
-    print('AKTUELLER ANTEIL ERNEUERBARER ENERGIEN: '+str(round(current_share_of_renewables * 100,2)) + ' %')
-    print('AKTUELLER AMPELSTATUS: '+color)
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    if format == 'text':
+        print('Quantile: ' + str(quantiles))
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print('REGION: '+str(country_code))
+        print('AKTUELLER ANTEIL ERNEUERBARER ENERGIEN: '+str(round(current_share_of_renewables * 100,2)) + ' %')
+        print('AKTUELLER AMPELSTATUS: '+color)
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    elif format == 'json':
+        print('"quantiles": ' + str(quantiles) + ', ')
+        print('"region": "'+ str(country_code) + '", ')
+        print('"current_share_of_renewables": '+str(round(current_share_of_renewables * 100,2)) + ', ')
+        print('"current_calculate_traffic_light_color": "'+color + '" ' )
+        print('}')
 
     if plotting == True:
         print_graph(df=renewables_df, quantiles=quantiles)
 
     return current_share_of_renewables, color
 
-main_app(token='xxx',
+parser = argparse.ArgumentParser("Electricity_Mix_Traffic_Light.py")
+parser.add_argument("--token", help="security token to access the API from EntsoePandas", required=True)
+parser.add_argument("--format", help="output format", choices=["text", "json"], default="text")
+parser.add_argument("--plot", help="plot chart", action='store_const', const=True, default=False)
+args = parser.parse_args()
+
+main_app(token=args.token,
              country_code='DE',
              no_of_quantiles=3,
              color_scheme=['RED', 'YELLOW', 'GREEN'],
              days_in_past=1,
              days_in_future=1,
              today_mode=True,
-             plotting=True)
+             plotting=args.plot,
+             format=args.format)
 
